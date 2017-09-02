113ef413cdb65a866c200b335dce6b9143a095b6
EryouHao <haoeryou@qq.com>
2017-09-03 01:07:39 +0800

    import iView

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import axios from 'axios'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 
 import App from './App'
 import router from './router'
@@ -10,7 +12,7 @@ if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.prototype.$db = db
-
+Vue.use(iView)
 /* eslint-disable no-new */
 new Vue({
   components: { App },

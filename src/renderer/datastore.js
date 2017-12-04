606df7c0ed89d6fdc50e1cec061f70be3a2c7f39
EryouHao <haoeryou@qq.com>
2017-12-04 10:01:09 +0800

    Update: adjust project structure

--- src/renderer/datastore.js
+++ src/renderer/datastore.js
@@ -1,8 +1,16 @@
 import path from 'path'
 import { remote } from 'electron'
+import fse from 'fs-extra'
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
-const adapter = new FileSync(path.join(remote.app.getPath('documents'), 'hve-blog/db/db.json'))
+// 检测是否已经存在 hve-blog文件夹，若不存在，则创建
+const documentsPath = remote.app.getPath('documents')
+fse.ensureDir(`${documentsPath}/hve-blog`)
+console.log('__static', __static)
+fse.copy(`${__static}/blog`, `${documentsPath}/hve-blog`)
+
+// db.json
+const adapter = new FileSync(path.join(documentsPath, 'hve-blog/db/db.json'))
 const db = low(adapter)
 
 db._.mixin({
@@ -12,6 +20,6 @@ db._.mixin({
   removeAll: (array) => (array = []),
 })
 
-const site = low(new FileSync(path.join(remote.app.getPath('documents'), 'hve-blog/db/site.json')))
+const site = low(new FileSync(path.join(documentsPath, 'hve-blog/db/site.json')))
 
 export { db, site }
